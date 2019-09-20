52c8435a8fc49942c5ecd64285d2ce8bcb6524bf
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-09-19 22:13:30 -0400

    Setting up testing

--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@
 Starter code to bootstrap node apis
 
 ## Useful commands
-    - ```docker-compose run api npm install``` to install all packages in package.json
+    - ```docker-compose run api npm install``` to install all dependencies in package.json
         **you must do this before attempting to build the image the first time**
-    - ```docker-compose run api install --save <packageName>``` to install a specific package
+    - ```docker-compose run api npm install --save <packageName>``` to install a specific package
     - ```docker-compose stop``` to stop all running containers
     - ```docker-compose down``` to remove containers and volumes
---
+++ tests/integration/api.test.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import request from 'supertest'
+import middleware from '../../src/middleware'
+import errorHandlers from '../../src/middleware/errorHandlers'
+import routes from '../../src/services'
+import { applyRoutes, applyMiddleware } from '../../src/utils'
+
+// an example of how you could test api endpoints
+describe('routes', () => {
+    let app;
+
+    beforeEach(()=>{
+        app = express()
+        applyMiddleware(middleware, app)
+        applyRoutes(routes, app)
+        applyMiddleware(errorHandlers, app)
+    })
+
+    describe('not found', () => {
+        it('should return a 404', async () => {
+            const res = await request(app).get('/not_found')
+            expect(res.status).toBe(404)
+        })
+    })
+})
